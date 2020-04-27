8858f0e15ccdf6937996e4002403136c192372aa
MUKUL KUMAR RAJAK <mukul18054@users.noreply.github.com>
2020-04-27 21:09:39 +0530

    added a lollipop graph for dead patient per disease

--- gui.py
+++ gui.py
@@ -1,4 +1,7 @@
+import seaborn as sns 
+
 import tkinter as tk
+
 from tkinter import filedialog, Text, font
 import os
 from PIL import ImageTk, Image
@@ -23,7 +26,7 @@ def showPatients():
 		label.append(i[0])
 		size.append(i[1])
 		explode.append(i[1]*0.02)
-
+	# print(explode)
 	figure1 = plt.Figure(figsize=(6,5), dpi=100)
 	ax1 = figure1.add_subplot(111)
 	canvas = FigureCanvasTkAgg(figure1, toplevel)
@@ -40,7 +43,8 @@ def showPatients():
 	for i in table[1]:
 		label.append(i[0])
 		size.append(i[1])
-
+	# print(label)
+	# print(explode)
 	figure1 = plt.Figure(figsize=(6,5), dpi=100)
 	ax = figure1.add_subplot(111)
 	canvas = FigureCanvasTkAgg(figure1, toplevel)
@@ -87,17 +91,52 @@ def showAdmin():
 	label=[]
 	size=[]
 	explode=[]
+	print(table)
+	print(table[1])
+
 	for i in table[1]:
 		label.append(i[0])
 		size.append(i[1])
 		explode.append(i[1]*0.02)
-
+	print(explode)
 	figure1 = plt.Figure(figsize=(6,5), dpi=100)
 	ax1 = figure1.add_subplot(111)
 	canvas = FigureCanvasTkAgg(figure1, toplevel)
 	canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
 	ax1.pie(size, labels=label, shadow=True,  autopct='%1.1f%%', explode=explode)
 	ax1.set_title("Distribution of Doctors according to Departments")
+
+
+	# Dead patients per Disease
+	query1="create table t select Disease_ID from Treatment inner join Patient on Patient.Treatment_ID= Treatment.Treatment_ID;"
+	query2="select Name, count(*) from t inner join Disease on t.Disease_ID=Disease.Disease_ID group by Name;"
+	query3="Drop table IF EXISTS t;" 
+	table3=sq.doQuery(query3)
+	table1=sq.doQuery(query1)
+	table2=sq.Query(query2)
+	table3=sq.doQuery(query3)
+	# print(table1)
+	# print(table2)
+	# print(table3)
+	label=[]
+	size=[]
+	for i in table2[1]:
+		label.append(i[0])
+		size.append(i[1])
+
+	# print(label)
+	# print(size)
+	figure1 = plt.Figure(figsize=(6,5), dpi=100)
+	ax = figure1.add_subplot(111)
+	canvas = FigureCanvasTkAgg(figure1, toplevel)
+	canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+
+	ax.vlines(x=label, ymin=0, ymax=size, color='firebrick', alpha=0.7, linewidth=2)
+	ax.scatter(x=label, y=size, color='firebrick', alpha=0.7)
+	ax.set_title('Lollipop Chart for Disease wise Patient Distribution')
+	ax.set_ylabel('Number of Patients')
+	ax.set_xticks(label)
+	ax.set_xticklabels(label, rotation=60, fontdict={'horizontalalignment': 'right'})
 
 def show3rdParty():
 
